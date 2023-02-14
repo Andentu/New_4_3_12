524afb62ff20202770913130bbe86dcac56e6dd2
Andentu <severalmaz97@gmail.com>
2023-02-14 20:14:54 +0300

    Убрал вывод ненужной информации. Добавил комментарии

--- New_4_3_12/Program.cs
+++ New_4_3_12/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var arr = new int[] { 5, 6, 2, 1 };
+            var arr = new int[] { 5, 2, 1 };
             int num;
             for (int i = 0; i < arr.Length; i++)
             {
@@ -14,17 +14,15 @@
                     // если Значение элемента первого [0] (в данном случае 5) больше, чем значение второго [1] элемента (в данном случае, 2), то выполняется код:
                     {
                         num = arr[i];
-                        Console.WriteLine("num = " + num);
-                        arr[i] = arr[j];
-                        Console.WriteLine("arr[i] = " + arr[i]);
-                        arr[j] = num;
-                        Console.WriteLine("arr[j] = " + arr[j]);
+                        arr[i] = arr[j];                        
+                        arr[j] = num;                        
                         Console.WriteLine();
-
-                    }
+                        // По сути, происходит перемена местами элементов массива
+                                            }
                 }
             }
-            foreach (var item in arr)
+            foreach (var item in arr) // Получает значение элемента массива из arr и записывает их в переменную var, после чего выводит на экран ее значение
+                                      // Значение получаемого элемента меняется по мере работы цикла for с содержимым if
             {
                 Console.Write(item);
             }
